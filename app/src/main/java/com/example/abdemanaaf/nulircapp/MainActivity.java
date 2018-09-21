d03df45b9f4f7eef587df5b46734f581a567ef71
Abdemanaaf Ghadiali <abdemanaafzghadiali@gmail.com>
2018-09-21 23:33:13 +0530

    Login Page Partially Completed
    Added a new login page with username and password. Transition from
    MainActivity.java to LoginPage.java. Changed the
    AndroidManifest.xml file to include the LoginPage.java file.
    
    * New LoginPage.java
    * New login_page.xml
    * Changed AndroidManifest.xml

--- app/src/main/java/com/example/abdemanaaf/nulircapp/MainActivity.java
+++ app/src/main/java/com/example/abdemanaaf/nulircapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.abdemanaaf.nulircapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView libraryApp = findViewById(R.id.libraryApp);
+        TextView toContinue = findViewById(R.id.toContinue);
+        ImageView logo = findViewById(R.id.logo);
+        RelativeLayout relativeLayout = findViewById(R.id.rl);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.my_transition);
+
+        libraryApp.startAnimation(animation);
+        logo.startAnimation(animation);
+        toContinue.startAnimation(animation);
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
